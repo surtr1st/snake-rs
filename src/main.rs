0aa3abe4eb3ca14f9f941d38e5b76137038d1341
ishi <berugen1st@gmail.com>
2023-10-28 02:36:24 +0700

    reduce hard-coded lines

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+pub mod snake;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
+use sdl2::rect::Point;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use snake::Snake;
 use std::thread;
 use std::time::Duration;
 
+const TITLE: &str = "Snake";
 const GRID_CELL: i32 = 25;
 const GRID_WIDTH: i32 = 32;
 const GRID_HEIGHT: i32 = 24;
+const SCREEN_SIZE: (u32, u32) = (800, 600);
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    let (width, height) = SCREEN_SIZE;
     let window = video_subsystem
-        .window("Snake", 800, 600)
+        .window(TITLE, width, height)
         .position_centered()
         .build()
         .unwrap();
@@ -27,27 +32,35 @@ fn main() {
     canvas.clear();
     canvas.present();
 
-    let snake = Rect::new(25, 25, 25, 25);
-
-    draw_grid(&mut canvas);
+    let mut snake = Snake::new(0, 0);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
+        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
+                Event::Quit { .. } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(keycode),
                     ..
-                } => break 'running,
+                } => match keycode {
+                    Keycode::Q => break 'running,
+                    Keycode::W => snake.go_up(),
+                    Keycode::S => snake.go_down(),
+                    Keycode::A => snake.go_left(),
+                    Keycode::D => snake.go_right(),
+                    _ => {}
+                },
                 _ => {}
             }
         }
+
+        draw_grid(&mut canvas);
+
         // The rest of the game loop goes here
-        canvas.fill_rect(snake).unwrap();
+        canvas.fill_rect(snake.rect(25, 25)).unwrap();
         canvas.set_draw_color(Color::GREEN);
 
-        //
         canvas.present();
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
