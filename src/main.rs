1aece38ba87708c786ab9dd92d793e4530d1fec0
ishi <berugen1st@gmail.com>
2023-10-27 18:02:27 +0700

    separate draw grid to function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use std::thread;
 use std::time::Duration;
 
@@ -27,28 +29,7 @@ fn main() {
 
     let snake = Rect::new(25, 25, 25, 25);
 
-    let window_width = (GRID_WIDTH * GRID_CELL) + 1;
-    let window_height = (GRID_HEIGHT * GRID_CELL) + 1;
-
-    let mut x = 0;
-    let mut y = 0;
-
-    let rows = GRID_WIDTH * GRID_CELL;
-    let cols = GRID_HEIGHT * GRID_CELL;
-    for _ in 0..rows {
-        x += GRID_CELL;
-        canvas.set_draw_color(Color::WHITE);
-        canvas
-            .draw_line(Point::new(x, 0), Point::new(x, window_height))
-            .unwrap();
-    }
-    for _ in 0..cols {
-        y += GRID_CELL;
-        canvas.set_draw_color(Color::WHITE);
-        canvas
-            .draw_line(Point::new(0, y), Point::new(window_width, y))
-            .unwrap();
-    }
+    draw_grid(&mut canvas);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
@@ -69,5 +50,30 @@ fn main() {
         //
         canvas.present();
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
+fn draw_grid(canvas: &mut Canvas<Window>) {
+    let window_width = (GRID_WIDTH * GRID_CELL) + 1;
+    let window_height = (GRID_HEIGHT * GRID_CELL) + 1;
+    let rows = GRID_WIDTH * GRID_CELL;
+    let cols = GRID_HEIGHT * GRID_CELL;
+
+    let mut x = 0;
+    let mut y = 0;
+
+    for _ in 0..rows {
+        x += GRID_CELL;
+        canvas.set_draw_color(Color::WHITE);
+        canvas
+            .draw_line(Point::new(x, 0), Point::new(x, window_height))
+            .unwrap();
+    }
+    for _ in 0..cols {
+        y += GRID_CELL;
+        canvas.set_draw_color(Color::WHITE);
+        canvas
+            .draw_line(Point::new(0, y), Point::new(window_width, y))
+            .unwrap();
     }
 }
