606a78a75e0a0d3ccdea4e1a57de6f4c49ede191
ishi <berugen1st@gmail.com>
2023-10-28 22:58:46 +0700

    update and handle errors

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ pub mod snake;
 use apple::Apple;
 use constants::{GRID_HEIGHT, GRID_WIDTH, SCREEN_SIZE, TITLE};
 use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::Canvas;
 use sdl2::render::TextureCreator;
-use sdl2::ttf::FontStyle;
+use sdl2::ttf::{FontStyle, Sdl2TtfContext};
 use sdl2::video::{Window, WindowContext};
 use snake::{Snake, SnakeDirection};
 use std::path::Path;
@@ -18,9 +19,11 @@ use std::time::Duration;
 
 use crate::constants::GRID_CELL;
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let ttf_context = sdl2::ttf::init()?;
+    sdl2::image::init(InitFlag::PNG)?;
 
     let (width, height) = SCREEN_SIZE;
     let window = video_subsystem
@@ -29,7 +32,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().build()?;
     let mut texture_creator = canvas.texture_creator();
 
     canvas.clear();
@@ -39,12 +42,17 @@ fn main() {
     let mut snake = Snake::new();
     let mut apple = Apple::spawn();
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
         canvas.clear();
         let score_indicate = format!("Score: {}", score);
-        draw_text(&mut canvas, &mut texture_creator, score_indicate);
-        draw_grid(&mut canvas);
+        draw_text(
+            &mut canvas,
+            &mut texture_creator,
+            &ttf_context,
+            score_indicate,
+        )?;
+        draw_grid(&mut canvas)?;
         for event in event_pump.poll_iter() {
             match event {
                 Event::KeyDown {
@@ -97,11 +105,17 @@ fn main() {
 
         // Snake color
         canvas.set_draw_color(Color::GREEN);
-        canvas.fill_rects(&rsnake).unwrap();
+        canvas.fill_rects(&rsnake)?;
 
         // Apple color
         canvas.set_draw_color(Color::RED);
-        canvas.fill_rect(rapple).unwrap();
+        render_texture(
+            &mut canvas,
+            &mut texture_creator,
+            String::from("textures/apple_25px.png"),
+            rapple,
+        )?;
+        canvas.fill_rect(rapple)?;
 
         // Background color
         canvas.set_draw_color(Color::BLACK);
@@ -109,9 +123,11 @@ fn main() {
         canvas.present();
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+
+    Ok(())
 }
 
-fn draw_grid(canvas: &mut Canvas<Window>) {
+fn draw_grid(canvas: &mut Canvas<Window>) -> Result<(), Box<dyn std::error::Error>> {
     let window_width = (GRID_WIDTH * GRID_CELL) + 1;
     let window_height = (GRID_HEIGHT * GRID_CELL) + 1;
     let rows = GRID_WIDTH * GRID_CELL;
@@ -123,17 +139,15 @@ fn draw_grid(canvas: &mut Canvas<Window>) {
     for _ in 0..rows {
         x += GRID_CELL;
         canvas.set_draw_color(Color::GREY);
-        canvas
-            .draw_line(Point::new(x, 0), Point::new(x, window_height))
-            .unwrap();
+        canvas.draw_line(Point::new(x, 0), Point::new(x, window_height))?;
     }
     for _ in 0..cols {
         y += GRID_CELL;
         canvas.set_draw_color(Color::GREY);
-        canvas
-            .draw_line(Point::new(0, y), Point::new(window_width, y))
-            .unwrap();
+        canvas.draw_line(Point::new(0, y), Point::new(window_width, y))?;
     }
+
+    Ok(())
 }
 
 fn detect_collision(target: &Rect, opponent: &Rect) -> bool {
@@ -171,18 +185,31 @@ fn resolve_overflow(target: &mut Snake) {
 fn draw_text(
     canvas: &mut Canvas<Window>,
     texture_creator: &mut TextureCreator<WindowContext>,
+    ttf_context: &Sdl2TtfContext,
     content: String,
-) {
-    let ttf_context = sdl2::ttf::init().unwrap();
+) -> Result<(), Box<dyn std::error::Error>> {
     let font_path = Path::new("fonts/Inter-V.ttf");
-    let mut font = ttf_context.load_font(font_path, 128).unwrap();
+    let mut font = ttf_context.load_font(font_path, 128)?;
     font.set_style(FontStyle::BOLD);
 
-    let surface = font.render(&content).blended(Color::WHITE).unwrap();
-    let texture = texture_creator
-        .create_texture_from_surface(surface)
-        .unwrap();
+    let surface = font.render(&content).blended(Color::WHITE)?;
+    let texture = texture_creator.create_texture_from_surface(surface)?;
     let target = Rect::new((SCREEN_SIZE.0 - 400) as i32, 0, 170, 50);
 
-    canvas.copy(&texture, None, Some(target)).unwrap();
+    canvas.copy(&texture, None, Some(target))?;
+
+    Ok(())
+}
+
+fn render_texture(
+    canvas: &mut Canvas<Window>,
+    texture_creator: &mut TextureCreator<WindowContext>,
+    path: String,
+    target: Rect,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let texture_path = Path::new(&path);
+    let texture = texture_creator.load_texture(&texture_path)?;
+    canvas.copy(&texture, None, Some(target))?;
+
+    Ok(())
 }
