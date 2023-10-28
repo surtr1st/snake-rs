8d4b8059c42cc6a21adc20fc6917bf448cbaecf3
ishi <berugen1st@gmail.com>
2023-10-28 17:01:26 +0700

    update Snake struct and add snake direction for handling movements

--- src/snake.rs
+++ src/snake.rs
@@ -1,42 +1,65 @@
 use sdl2::rect::Rect;
 
-#[derive(Clone, Copy)]
-pub struct Snake(i32, i32);
+enum SnakeDirection {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+pub struct Snake {
+    x: i32,
+    y: i32,
+    direction: SnakeDirection,
+}
 
 impl Snake {
     pub fn new(x: i32, y: i32) -> Self {
-        Snake(x, y)
+        Snake {
+            x,
+            y,
+            direction: SnakeDirection::Right,
+        }
     }
 
     pub fn rect(&self, size: u32) -> Rect {
-        Rect::new(self.0, self.1, size, size)
+        Rect::new(self.x, self.y, size, size)
     }
 
     pub fn position(&self) -> (i32, i32) {
-        (self.0, self.1)
+        (self.x, self.y)
     }
 
     pub fn set_x(&mut self, x: i32) {
-        self.0 = x;
+        self.x = x;
     }
 
     pub fn set_y(&mut self, y: i32) {
-        self.1 = y;
+        self.y = y;
     }
 
     pub fn go_left(&mut self) {
-        self.0 -= 25;
+        self.direction = SnakeDirection::Left;
     }
 
     pub fn go_right(&mut self) {
-        self.0 += 25;
+        self.direction = SnakeDirection::Right;
     }
 
     pub fn go_up(&mut self) {
-        self.1 -= 25;
+        self.direction = SnakeDirection::Up;
     }
 
     pub fn go_down(&mut self) {
-        self.1 += 25;
+        self.direction = SnakeDirection::Down;
+    }
+
+    pub fn wriggle(&mut self) {
+        match self.direction {
+            SnakeDirection::Up => self.y -= 25,
+            SnakeDirection::Down => self.y += 25,
+            SnakeDirection::Left => self.x -= 25,
+            SnakeDirection::Right => self.x += 25,
+        }
     }
 }
